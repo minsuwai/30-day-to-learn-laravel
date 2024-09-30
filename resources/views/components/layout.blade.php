e7176a06352f31a851603a640ddcfeaccf0f9ccc
minsuwai <mswwai15@gmail.com>
2024-10-01 00:10:10 +0630

    feat: add tailwind css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,9 +9,9 @@
 
 <body>
     <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
     </nav>
 
     {{ $slot }}
