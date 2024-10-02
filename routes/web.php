b531501fd7bd4befa9f8a282c68cd015285f675d
minsuwai <mswwai15@gmail.com>
2024-10-02 23:18:30 +0630

    feat: day-6 view data and route wildcards

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,4 +13,50 @@ Route::get('/about', function () {
 
 Route::get('/contact', function () {
     return view('contact');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Manager',
+                'salary' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Developer',
+                'salary' => '$30,000'
+            ]
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Manager',
+            'salary' => '$40,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Developer',
+            'salary' => '$30,000'
+        ]
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
