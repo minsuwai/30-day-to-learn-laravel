889409b6d5c107383c24adcb8cb159b625f957d4
minsuwai <mswwai15@gmail.com>
2024-10-04 00:19:32 +0630

    feat: day-7 autoloading/Models/Namespaces

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -17,46 +18,12 @@ Route::get('/contact', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Manager',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Developer',
-                'salary' => '$30,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Manager',
-            'salary' => '$40,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Developer',
-            'salary' => '$30,000'
-        ]
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
