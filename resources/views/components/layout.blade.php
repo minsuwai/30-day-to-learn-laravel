957d580f7d9d42bf650edec32974266409a15b2d
minsuwai <mswwai15@gmail.com>
2024-10-24 23:51:43 +0630

    feat: day-26 vite asset bundling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 </head>
 
 <body class="h-full">
